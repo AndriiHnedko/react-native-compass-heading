b5e75edcda03620d6e36f0b3057927f17504206b
andriihnedko <andrii.hnedko@gmail.com>
2025-02-12 16:49:24 +0200

    chore: add missing types

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,36 @@
-import { NativeModules, NativeEventEmitter, Platform } from 'react-native';
+import {
+  NativeModules,
+  NativeEventEmitter,
+  Platform,
+  type NativeModule,
+} from 'react-native';
+
+export interface HeadingData {
+  heading: number;
+  accuracy: number;
+}
+
+export interface CompassHeadingModule extends NativeModule {
+  start(
+    updateRate: number,
+    callback: (data: HeadingData) => void
+  ): Promise<void>;
+
+  stop(): Promise<void>;
+
+  hasCompass(): Promise<boolean>;
+}
 
 const LINKING_ERROR =
   `The package 'react-native-compass-heading' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  Platform.select({
+    ios: "- You have run 'pod install'\n",
+    default: '',
+  }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
 
-const CompassHeading = NativeModules.CompassHeading
+const CompassHeading: CompassHeadingModule = NativeModules.CompassHeading
   ? NativeModules.CompassHeading
   : new Proxy(
       {},
@@ -21,14 +45,9 @@ let listener: { remove: () => any } | null = null;
 
 let _start = CompassHeading.start;
 
-type dataType = {
-  heading: number;
-  accuracy: number;
-};
-
 CompassHeading.start = async (
   update_rate: number,
-  callback: (data: dataType) => void
+  callback: (data: HeadingData) => void
 ) => {
   if (listener) {
     await CompassHeading.stop(); // Clean up previous listener
@@ -37,7 +56,7 @@ CompassHeading.start = async (
   const compassEventEmitter = new NativeEventEmitter(CompassHeading);
   listener = compassEventEmitter.addListener(
     'HeadingUpdated',
-    (data: dataType) => {
+    (data: HeadingData) => {
       callback(data);
     }
   );
